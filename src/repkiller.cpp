f92f01d1165c2a136b1a33eb2f0124931ee1552c
Carles-Bordas <carles.bordas@uma.es>
2017-10-18 00:17:40 +0200

    Code cleanup and new repetition detection system

--- src/repkiller.cpp
+++ src/repkiller.cpp
@@ -6,15 +6,13 @@
 #include <ctype.h>
 #include <pthread.h>
 #include <float.h>
-#include <set>
 #include "structs.h"
 #include "commonFunctions.h"
 #include "comparisonFunctions.h"
-#include "evolutionaryEventsFunctions.h"
 
 #define LOGT(s) fprintf(stdout, "[INFO] %s T = %e\n", s, (double)(end-begin)/CLOCKS_PER_SEC);
 
-using namespace std;
+#define LOGI(s) fprintf(stdout, "[INFO] %s\n", s);
 
 uint64_t total_bytes_in_use = 0;
 int DEBUG_ACTIVE = 0;
@@ -22,34 +20,22 @@ int HARD_DEBUG_ACTIVE = 0;
 
 void print_all();
 void init_args(int argc, char ** av, FILE ** multifrags, char ** out_file_base_path,
-    char * path_frags, uint64_t * ht_size);
+    char * path_frags);
 int main(int ac, char **av) {
-    //Iterator
-    uint64_t i;
-    //Number of frags loaded, number of sequences loaded
-    uint64_t total_frags, n_files;
-    //Last ID of synteny lists
-    uint64_t last_s_id;
-    //Array of fragments to hold them all, pointer to free when changing pointer
-    struct FragFile * loaded_frags = NULL;
-    //Clocks to measure time
+    // Clocks to measure time
     clock_t begin, end;
-    //The sequences manager to store ids, lengths, etc
-    sequence_manager * seq_manager = new sequence_manager();
-    //The number of iterations to trimm
+    // The sequences manager to store ids, lengths, etc
+    sequence_manager seq_manager;
+
     char multifrags_path[512]; multifrags_path[0] = '\0';
-    //Initial hash table size (divisor of the longest sequence)
-    uint64_t ht_size = 100; //Default
-    //Default behaviour is the trimmed frags do not already exist
     char * out_file_base_path = new char[MAX_LINE];
-    //
     char * out_file_name = new char[MAX_LINE];
 
-    //Open frags file, lengths file and output files %%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Open frags file, lengths file and output files %%%%%%%%%%%%%%%%%%%%%%%%%%%
     FILE * frags_file = NULL, * lengths_file = NULL;
-    init_args(ac, av, &frags_file, &out_file_base_path, multifrags_path, &ht_size);
+    init_args(ac, av, &frags_file, &out_file_base_path, multifrags_path);
 
-    //Concat .lengths to path of multifrags %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Concat .lengths to path of multifrags %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     char path_lengths[READLINE];
     path_lengths[0]='\0';
     strcpy(path_lengths, multifrags_path);
@@ -57,82 +43,33 @@ int main(int ac, char **av) {
     lengths_file = fopen64(path_lengths, "rb");
     if(lengths_file == NULL) terror("Could not open input lengths file");
 
-    //Load lengths and substract accumulated length %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Load fragments into array %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    LOGI("Loading fragments into memory...");
     begin = clock();
-    n_files =  seq_manager->load_sequences_descriptors(lengths_file);
+    FragmentsDatabase frag_db(frags_file, lengths_file, seq_manager);
     end = clock();
-    LOGT("Loaded sequence descriptors.");
+    LOGT("Fragments loaded into memory succesfully!");
+    fclose(frags_file);
     fclose(lengths_file);
 
-    //Load fragments into array %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Generate fragment groups %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    LOGI("Generating fragment groups...");
     begin = clock();
-    load_fragments_local(frags_file, &total_frags, &loaded_frags);
+    FGList * efrags_groups = new FGList();
+    generate_fragment_groups(frag_db, efrags_groups, 0.8, 8.0);
     end = clock();
-    print_memory_usage();
-    LOGT("Loaded fragments into memory.");
-    fclose(frags_file);
+    LOGT("Fragment groups generated succesfully!");
 
-    //Frags to blocks conversion %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    // Save results in file %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    LOGI("Saving results into secondary memory...");
     begin = clock();
-    memory_pool * mp = new memory_pool(POOL_SIZE);
-    uint64_t max_len_sequence = seq_manager->get_maximum_length();
-    uint64_t coord_aux;
-    // ram debug over here
-    hash_table * ht = new hash_table(mp, max_len_sequence/ht_size, seq_manager, max_len_sequence);
-    for(i=0;i<total_frags;i++){
-        //Switch coordinates of reversed fragments. This can only be done at the end of trimming and not meanwhile!
-        if(loaded_frags[i].strand == 'r'){ coord_aux = loaded_frags[i].yStart; loaded_frags[i].yStart = loaded_frags[i].yEnd; loaded_frags[i].yEnd = coord_aux;}
-        ht->insert_block(&loaded_frags[i]);
-        print_load(100.0 * i / total_frags);
-    }
-    //ht->print_hash_table(2);
-
-    compute_order_of_blocks(ht, n_files);
-    end = clock();
-    //fprintf(stdout, "[INFO] Insertion of fragments into hash table completed. Load factor = %e. T = %e\n", ht->get_load_factor(), (double)(end-begin)/CLOCKS_PER_SEC);
-    print_memory_usage();
-    LOGT("Insertion of fragments into hash table completed.");
-
-    //Generate synteny blocks %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    begin = clock();
-    auto synteny_block_list = compute_synteny_list(ht, n_files, mp, &last_s_id);
-    //traverse_synteny_list_and_write(synteny_block_list, n_files, "init");
-    //traverse_synteny_list(synteny_block_list);
-    end = clock();
-    print_memory_usage();
-    LOGT("Generated synteny blocks.");
-
-    // Redo synteny blocks into frags group list %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    begin = clock();
-    auto frags_groups = redo_synteny_system(synteny_block_list);
-    end = clock();
-    //print_frags_grops_list(ptr_fgl);
-    print_memory_usage();
-    LOGT("Frags groups list created.");
-
-    // Extend frags groups using parameter 'e' %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    begin = clock();
-    FGList efrags_groups;
-    extend_groups(*frags_groups, &efrags_groups);
-    end = clock();
-    LOGT("Extensions calculated.");
-
-    begin = clock();
-    save_all_frag_pairs(out_file_base_path, seq_manager, efrags_groups);
+    save_all_frag_pairs(out_file_base_path, seq_manager, *efrags_groups);
     end = clock();
     LOGT("Fragments saved into csv file.");
 
-    std::free(loaded_frags);
-
+    // Final cleanup %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     delete [] out_file_base_path;
     delete [] out_file_name;
-
-    delete ht;
-    delete mp;
-    delete seq_manager;
-
-    return 0;
 }
 
 void print_all(){
@@ -144,7 +81,7 @@ void print_all(){
 }
 
 void init_args(int argc, char ** av, FILE ** multifrags, char ** out_file_base_path,
-    char * path_frags, uint64_t * ht_size){
+    char * path_frags){
 
     int pNum = 0;
     while(pNum < argc){
@@ -163,10 +100,6 @@ void init_args(int argc, char ** av, FILE ** multifrags, char ** out_file_base_p
         if(strcmp(av[pNum], "-out") == 0){
             if(av[pNum+1]==NULL) terror("ERR¿?");
             strcpy(*out_file_base_path, av[pNum+1]);
-        }
-        if(strcmp(av[pNum], "-hash_table_divisor") == 0){
-            *ht_size = (uint64_t) atoi(av[pNum+1]);
-            if(*ht_size < 1) terror("The hash table divisor must be one at least");
         }
         pNum++;
     }
--- src/structs.h
+++ src/structs.h
@@ -1,4 +1,5 @@
 #pragma once
+#pragma pack(1)
 
 #include <inttypes.h>
 #include <iostream>
@@ -8,8 +9,6 @@
 #include <forward_list>
 
 using namespace std;
-
-#pragma pack(push, 1)
 
 #define SEQ_REALLOC 5000000
 //#define MAX_READ_SIZE 5000 //Maximum length of read to have a portion of the table allocated
@@ -65,14 +64,7 @@ extern uint64_t total_bytes_in_use;
 
 //Class prototypes
 class memory_pool;
-class hash_table;
 class sequence_manager;
-
-//Struct prototypes
-struct synteny_list;
-
-//Enum of events
-enum Event { inversion, duplication, transposition, insertion, deletion, indel, none, concatenation}; // Indel is generic for insertion or deletion, and has to be decided
 
 //Struct for FragHits, af2png and leeFrag programs
 struct FragFile {
@@ -116,40 +108,6 @@ struct FragFile {
     long double evalue;
 };
 
-//Struct for NW two rows
-struct cell{
-	int64_t score;
-	uint64_t xe;
-	uint64_t ye;
-	uint64_t xs;
-	uint64_t ys;
-	uint64_t igaps;
-	uint64_t egaps;
-	uint64_t ident;
-};
-
-//Struct for full NW
-struct cell_f{
-    int64_t score;
-    uint64_t xfrom;
-    uint64_t yfrom;
-};
-struct positioned_cell{
-    int64_t score;
-    uint64_t xpos;
-    uint64_t ypos;
-};
-struct best_cell{
-    struct positioned_cell c;
-    uint64_t j_prime;
-};
-typedef struct{
-    uint64_t identities;
-    uint64_t length;
-    uint64_t igaps;
-    uint64_t egaps;
-} BasicAlignment;
-
 //Sequence descriptor
 typedef struct sequence{
     uint64_t id;    //Label of the sequence
@@ -159,17 +117,6 @@ typedef struct sequence{
     uint64_t n_frags; //Number of fragments that the sequence had
     char * seq; //DNA sequence
 } Sequence;
-
-// Struct used to compute the alignment between two hits
-typedef struct quickfrag{
-    uint64_t x_start;
-    uint64_t y_start;
-    uint64_t t_len;
-    int64_t diag;
-    long double sim;
-    Sequence * x;
-    Sequence * y;
-} Quickfrag;
 
 // A collection of fragments
 typedef struct frags_list{
@@ -233,6 +180,7 @@ typedef struct synteny_list{
     uint64_t id;
 } Synteny_list;
 
+/*
 class FragsGroup {
 private:
   set<FragFile*> groups;
@@ -245,17 +193,11 @@ public:
   void clear() { groups.clear(); xstarts.clear(); ystarts.clear(); };
   set<FragFile*>::iterator begin() { return groups.begin(); }
   set<FragFile*>::iterator end()   { return groups.end(); }
-};
+};*/
 
-typedef vector<FragsGroup> FGList;
+typedef forward_list<FragFile*> FragsGroup;
+typedef forward_list<FragsGroup*> FGList;
 
-struct triplet{
-    Synteny_list * A;
-    Synteny_list * B;
-    Synteny_list * C;
-    struct triplet * next;
-    Event etype;
-};
 //Class for allocating memory only once and requesting particular amounts of bytes
 class memory_pool{
 
@@ -287,214 +229,20 @@ typedef struct wordbucket{
     struct wordbucket * next;
 } Wordbucket;
 
-// For pthreads alignment of NW
-struct alignment_arguments{
-    char * seq_A;
-    Sequence * s_x;
-    Sequence * s_y;
-    uint64_t start_A;
-    uint64_t end_A;
-    char * seq_B;
-    uint64_t start_B;
-    uint64_t end_B;
-    int64_t iGap;
-    int64_t eGap;
-    struct cell * mc;
-    struct cell * f0;
-    struct cell * f1;
-    struct cell * alignment;
-    struct cell * alignment_reverse;
-    char * seq_for_reverse;
-};
-
-typedef struct annotation{
-    uint64_t start;
-    uint64_t end;
-    char strand;
-    char * product; //Requires hard copy
-} Annotation;
-
 // Sequence class management
 class sequence_manager
 {
 private:
     Sequence * sequences;   //A pointer to the sequences
     uint64_t n_sequences;   //Number of sequences
-    char * path_annotations;
-    Annotation ** annotation_lists;
-    uint64_t * n_annotations;
-
 public:
     sequence_manager();
-    void set_path_annotations(char * p){ path_annotations = p; }
-    char * get_path_annotations(){ return this->path_annotations; }
     uint64_t load_sequences_descriptors(FILE * lengths_file);
-    Sequence * get_sequence_by_label(uint64_t label);
-    uint64_t get_maximum_length();
-    uint64_t get_number_of_sequences() { return n_sequences; }
-    void print_sequences_data();
-    void print_annotations();
-    Annotation * get_annotation_list(uint64_t label){ return this->annotation_lists[label]; }
-    uint64_t get_annotations_number_in_list(uint64_t label){ return this->n_annotations[label]; }
-    void read_dna_sequences(char * paths_to_files);
-    void read_annotations();
-    void print_sequence_region(FILE * fout, uint64_t label, uint64_t from, uint64_t to);
+    Sequence * get_sequence_by_label(uint64_t label) const;
+    uint64_t get_maximum_length() const;
+    uint64_t get_number_of_sequences() const { return n_sequences; }
+    void print_sequences_data() const;
     ~sequence_manager();
-};
-
-//Hash-table class for Blocks management
-class hash_table
-{
-
-private:
-	Bucket ** ht; // the table itself
-	memory_pool * mp;
-    uint64_t ht_size; //Size for the hash table //Init size
-    uint64_t n_buckets; //To compute the load factor
-    uint64_t n_entries; //Used up entries
-    double key_factor; //To partitionate the space by the largest genome
-    uint64_t computed_sizeof_block; //Avoid overcomputing
-    uint64_t computed_sizeof_frags_list; //Avoid overcomputing
-    sequence_manager * sequences;
-
-public:
-    hash_table(memory_pool * main_mem_pool, uint64_t init_size, sequence_manager * sequences, uint64_t highest_key);
-	void insert_block(struct FragFile * f);
-    Bucket * get_key_at(uint64_t pos){ if(pos < ht_size && pos >= 0) return ht[pos]; else return NULL; } //Returns a reference to the key by absolute position
-    Bucket * get_value_at(uint64_t pos); //Returns a reference to the key computed from the hash of x_pos
-    Block * get_block_from_frag(struct FragFile * f, int x_or_y);
-    //double get_load_factor(){ return (double)ht_size/n_buckets;}
-    uint64_t get_size(){ return ht_size; }
-    void print_hash_table(int print);
-    Bucket * get_iterator(){ return ht[0];}
-    Block * get_previous_block(Block * b);
-    Block * get_next_block(Block * b);
-    void release_temp_last_blocks();
-    void remove_block(Block * b);
-    void write_blocks_and_breakpoints_to_file(FILE * out_blocks, FILE * out_breakpoints);
-    Block ** last_blocks; // Used to generate the double link between blocks
-
-private:
-	uint64_t compute_hash(uint64_t key);
-    void insert_x_side(struct FragFile * f);
-    void insert_y_side(struct FragFile * f);
-};
-
-// Dictionary class to handle words and hits between sequences
-class dictionary_hash{
-private:
-    Wordbucket ** words;
-    Wordbucket ** list; //To retrieve all possible sequences to align with
-    uint64_t list_allocs; //how many times the list was reallocated
-    uint64_t n_list_pointers; //The number of pointers in the current list
-    uint64_t ht_size;
-    uint32_t kmer_size;
-    uint64_t computed_sizeofwordbucket;
-    double key_factor;
-    memory_pool * mp;
-public:
-    dictionary_hash(uint64_t init_size, uint64_t highest_key, uint32_t kmer_size);
-    Wordbucket ** put_and_hit(char * kmer, char strand, uint64_t position, Block * b);
-    uint64_t get_candidates_number(){ return this->n_list_pointers;}
-    void clear();
-    ~dictionary_hash();
-private:
-    uint64_t compute_hash(char * kmer);
-};
-
-class markdown_event_hash{
-private:
-    triplet ** array;
-    uint64_t size;
-    memory_pool * mp;
-public:
-    markdown_event_hash(uint64_t size);
-    void put(triplet * k);
-    triplet * find_triplet(triplet * k);
-    void remove(triplet * k);
-    ~markdown_event_hash();
-
-private:
-    uint64_t compute_hash(triplet * k);
-};
-
-typedef struct slist{
-    Sequence * s;
-    struct slist * next;
-} Slist;
-
-
-
-// There will be one strand matrix per synteny block to compute if a synteny list might have a reversion
-class strand_matrix
-{
-
-public:
-    unsigned char ** sm; // For strands
-    uint64_t * sm_orders; // For orders
-    uint64_t n_seqs;
-    uint64_t squared_sequences;
-    uint64_t acu_frags_forward;
-    uint64_t acu_frags_reverse;
-
-    strand_matrix(uint64_t sequences);
-    //Unsure about this one below
-    int is_block_reversed(uint64_t block_number);
-    int do_forwards_require_less_changes(uint64_t genome);
-    int get_strands_type();
-    unsigned char get_strands(uint64_t l1, uint64_t l2);
-    void set_strands(uint64_t l1, uint64_t l2, unsigned char v);
-    uint64_t get_frags_forward(){ return this->acu_frags_forward; }
-    uint64_t get_frags_reverse(){ return this->acu_frags_reverse; }
-    bool compare_with_other_matrix(strand_matrix * m);
-    bool compare_order_with_other_matrix(strand_matrix * m);
-    uint64_t get_order(uint64_t l1) { return this->sm_orders[l1]; }
-    void reset();
-    void add_fragment_strands(Synteny_list * sbl);
-    void print_strand_matrix();
-    void print_strand_matrix_order();
-    ~strand_matrix();
-};
-
-// Structs to pass events and handle them in one function
-struct e_inversion{
-    Block * inv; //A block with inversion
-};
-
-struct e_duplication{
-    Block * orig; //The original block from which the duplication "duplicated"
-    Block * dup; //A block that has been detected as duplication
-};
-
-struct e_transposition{
-    Block * before_trans;
-    Block * transposed;
-};
-
-struct e_insertion{
-    Block * insertion;
-};
-
-struct e_deletion{
-    Block * deletion;
-    uint64_t removed;
-};
-
-struct e_concatenation{
-    Block * involved;
-    uint64_t coords1;
-    uint64_t coords2;
-};
-
-// Struct to modify blocks given previous rearrangments
-struct rearrangement{
-    int64_t mod_coordinates; //coordinate offset to add
-    int64_t mod_order; //order offset to add
-    uint64_t b1_id; //Range start of blocks affected
-    uint64_t b2_id; //Range end of blocks affected
-    uint64_t ends_at; //Ending id
-    uint64_t type;
-    uint64_t affects_who; //-1 for all, either specify genome label
 };
 
 // Bucket of linked list for heuristic sorting
@@ -519,83 +267,29 @@ class heuristic_sorted_list{
     ~heuristic_sorted_list();
 };
 
-// Queue-like class to handle all operations that have to be done
-class events_queue{
+class FragmentsDatabase {
 private:
-    std::list<rearrangement> * rea_queue;
-    std::list<rearrangement>::iterator rea_itera;
-    rearrangement * aggregated_r;
-    uint64_t n_sequences;
+  FragFile * loaded_frags;
+  uint64_t num_frags = 0;
 public:
-    events_queue(uint64_t n_sequences);
-    void insert_event(rearrangement r);
-    uint64_t get_queue_size(){ return this->rea_queue->size(); }
-    rearrangement * get_aggregated_event(Block * b, uint64_t s_id);
-    void begin_iterator(){ this->rea_itera = this->rea_queue->begin(); }
-    void print_queue();
-    ~events_queue();
+  FragmentsDatabase(FILE * frags_file, FILE * lengths_file, sequence_manager & seq_manager);
+  FragFile * getFragAt(size_t index) const { return &loaded_frags[index]; }
+  uint64_t getTotalFrags() const { return num_frags; }
+  ~FragmentsDatabase();
 };
 
-// Class to register events and write them to file
-class ee_log{
+class SequenceOcupationList {
 private:
-    FILE * logfile;
-    char * write_buffer;
-    uint64_t write_index;
-    uint64_t event_count;
-    char tmp[256];
-    char path[MAX_LINE];
-
+  double len_pos_ratio, threshold;
+  struct Ocupation {
+    uint64_t center;
+    uint64_t length;
+    FragsGroup * group;
+    Ocupation(uint64_t center, uint64_t length, FragsGroup * group) : center(center), length(length), group(group) {};
+  };
+  std::forward_list<Ocupation> ocupations;
 public:
-    ee_log(FILE * logfile, char * path);
-    void register_event(Event e, void * event_data);
-    void force_write();
-    ~ee_log();
-
-private:
-    void write(const char * data);
-
-};
-
-struct Event_handling{
-    Event type_of_event;
-    bool * genomes_affected;
-};
-struct Indel_handling{
-    Synteny_block * concat;
-    uint64_t * bp_lengths;
-    uint64_t n_sequences;
-};
-
-// Struct for reducing parameters in multiple alignment function
-struct arguments_multiple_alignment{
-    sequence_manager * seq_man;
-    char ** seq_for_reverse;
-    Synteny_list * sbl;
-    Quickfrag ** qfmat;
-    unsigned char ** qfmat_state;
-    int iGap;
-    int eGap;
-    struct cell ** mc;
-    struct cell ** f0;
-    struct cell ** f1;
-    pthread_t * threads;
-    double ** submat;
-    memory_pool * mp;
-    // For full NW
-    char * aux_dummy_sequence; // The one that is not used in the backtrackings
-    char ** recon_X;
-    uint64_t * sequence_ids;  // Tells which one of the seqs in seq_X is after aligning
-    char ** recon_Y;
-    char ** recon_Z;
-    char ** seq_X;
-    char ** seq_Y;
-    char ** seq_Z;
-    int64_t * cell_path_y;
-    struct positioned_cell * mc_f;
-    struct cell_f ** table_f;
-    char * writing_buffer_alignment;
-    long double window;
-    uint64_t n_sequences;
-
+  SequenceOcupationList(double len_pos_ratio, double threshold);
+  FragsGroup * get_associated_group(uint64_t center, uint64_t length) const;
+  void insert(uint64_t center, uint64_t length, FragsGroup * group) { ocupations.push_front(Ocupation(center, length, group)); };
 };
